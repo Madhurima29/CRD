38246e13a1af62bbe48f8a53404d107112d0ee0b
sreekanthmettles <34197514+sreekanthmettles@users.noreply.github.com>
2020-02-13 06:57:31 -0800

    Update OrderSelectService.java

--- server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/r4/OrderSelectService.java
+++ server/src/main/java/org/hl7/davinci/endpoint/cdshooks/services/crd/r4/OrderSelectService.java
@@ -61,15 +61,11 @@ import org.springframework.web.client.RestTemplate;
 public class OrderSelectService extends CdsService<OrderSelectRequest> {
 
     public static final String ID = "order-select-crd";
-    public static final String TITLE = "order-review Coverage Requirements Discovery";
+    public static final String TITLE = "order-select Coverage Requirements Discovery";
     public static final Hook HOOK = Hook.ORDER_SELECT;
     public static final String DESCRIPTION
-            = "Get information regarding the coverage requirements for durable medical equipment";
+            = "Get information regarding the coverage requirements for Service Requests";
     public static final List<PrefetchTemplateElement> PREFETCH_ELEMENTS = Arrays.asList(
-            CrdPrefetchTemplateElements.DEVICE_REQUEST_BUNDLE,
-            CrdPrefetchTemplateElements.SUPPLY_REQUEST_BUNDLE,
-            CrdPrefetchTemplateElements.NUTRITION_ORDER_BUNDLE,
-            CrdPrefetchTemplateElements.MEDICATION_REQUEST_BUNDLE,
             CrdPrefetchTemplateElements.SERVICE_REQUEST_BUNDLE);
     public static final FhirComponents FHIRCOMPONENTS = new FhirComponents();
     static final Logger logger = LoggerFactory.getLogger(OrderSelectService.class);
